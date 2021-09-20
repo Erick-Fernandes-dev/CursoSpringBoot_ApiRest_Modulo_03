e54a6552908f2cb2719b386f67f36722a1f8dc54
ErickFernandes <erick.farias@dcx.ufpb.br>
2021-09-20 19:44:40 -0300

    Project with spring boot

--- src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
+++ src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
@@ -2,17 +2,35 @@ package io.github.ErickFernandes_Algnologia.rest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.github.ErickFernandes_Algnologia.domain.entity.Cliente;
+
 @Controller
-@RequestMapping("/api/clientes")
+//@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+        value = {"/api/clientes/hello/{nome}", "/api/hello"},
+        method = RequestMethod.GET,
+        consumes = { "application/json", "application/xml "},
+        produces = { "application/json", "application/xml"}
+        )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+    public String helloCliente(
+        @PathVariable("nome") String nomeCliente,
+        @RequestBody Cliente Cliente
+        ) {
         return String.format("Hello %s ", nomeCliente);
     }
+
+    /*
+    * @RequestMapping --:>
+    *
+    *
+    * */
+
 }
