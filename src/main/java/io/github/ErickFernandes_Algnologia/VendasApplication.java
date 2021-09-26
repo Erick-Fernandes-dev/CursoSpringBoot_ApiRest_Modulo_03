37e6640443a8ea375221b0048a77bddbc540cd97
ErickFernandes <erick.farias@dcx.ufpb.br>
2021-09-25 21:38:40 -0300

    Project: Api Rest with spring boot

--- src/main/java/io/github/ErickFernandes_Algnologia/VendasApplication.java
+++ src/main/java/io/github/ErickFernandes_Algnologia/VendasApplication.java
@@ -1,11 +1,28 @@
 package io.github.ErickFernandes_Algnologia;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.ErickFernandes_Algnologia.domain.entity.Cliente;
+import io.github.ErickFernandes_Algnologia.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+        return args -> {
+
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+            
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
+++ src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
@@ -1,6 +1,10 @@
 package io.github.ErickFernandes_Algnologia.rest.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,29 +12,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.github.ErickFernandes_Algnologia.domain.entity.Cliente;
+import io.github.ErickFernandes_Algnologia.domain.repository.Clientes;
 
 @Controller
-//@RequestMapping("/api/clientes")
+// @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-        value = {"/api/clientes/hello/{nome}", "/api/hello"},
-        method = RequestMethod.GET,
-        consumes = { "application/json", "application/xml "},
-        produces = { "application/json", "application/xml"}
-        )
-    @ResponseBody
-    public String helloCliente(
-        @PathVariable("nome") String nomeCliente,
-        @RequestBody Cliente Cliente
-        ) {
-        return String.format("Hello %s ", nomeCliente);
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
     }
 
     /*
-    * @RequestMapping --:>
-    *
-    *
-    * */
+     * @RequestMapping( value = {"/api/clientes/hello/{nome}", "/api/hello"}, method
+     * = RequestMethod.GET, consumes = { "application/json", "application/xml "},
+     * produces = { "application/json", "application/xml"} )
+     */
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody // Vai transformar esse retorno em um objeto do tipo Json
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());//200
+        }
+
+        return ResponseEntity.notFound().build();//retorna o erro 404
+    }
+
+    /*
+     * @RequestMapping --:> 
+     * responseEntity --> e um objeto que ele representa o corpo da resposta
+     *
+     */
 
 }
