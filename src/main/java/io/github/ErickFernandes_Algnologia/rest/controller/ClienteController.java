73908c13d7f46c6353a3c23cf0b0778969461371
ErickFernandes <erick.farias@dcx.ufpb.br>
2021-10-05 15:56:02 -0300

    Project: Api Rest in Spring Boot

--- src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
+++ src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,11 +52,38 @@ public class ClienteController {
 
         return ResponseEntity.ok(clienteSalvo);
 
-
-
-
-
     };
+
+    public ResponseEntity delete(@PathVariable Integer id) {
+
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+
+        return ResponseEntity.notFound().build();
+
+
+    }
+
+    @PutMapping("/api/clientes/{id}")// o put ele é usado para atualizar integralmente
+    @ResponseBody
+    public ResponseEntity update(
+        @PathVariable Integer id, 
+        @RequestBody Cliente  cliente) {
+
+        return clientes
+        .findById(id)
+        .map( clienteExistente -> {
+            cliente.setId(clienteExistente.getId());
+            clientes.save(cliente);
+            return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());//supplier
+
+    }
 
     /*
      * @RequestMapping --:> 
@@ -63,6 +91,13 @@ public class ClienteController {
      * 
      * @RequestBody --> é quando a gente recebe uma requisição, ou seja, o que vai receber
      * @ResonseBody --> é quando a gente retorna uma resposta, ou seja, o que vai retornar
+     * 
+     * noContent --> é quando vai retornar nada no corpo da requisição
+     * 
+     * ResponseEntity --> Significa representar toda a resposta HTTP
+     * 
+     * @PathVariable --> a anotação pode ser usada para manipular variáveis ​​de modelo no 
+     * mapeamento de URI de solicitação e defini-las como parâmetros de método.
      *
      */
 
