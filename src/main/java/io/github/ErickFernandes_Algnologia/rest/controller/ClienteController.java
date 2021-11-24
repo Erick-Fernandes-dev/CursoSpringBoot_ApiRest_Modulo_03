6449522a01c5fc5812dc58fc3dba215111b01cc1
ErickFernandes <erick.farias@dcx.ufpb.br>
2021-11-23 21:13:09 -0300

    Update: Api Rest with Spring boot

--- src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
+++ src/main/java/io/github/ErickFernandes_Algnologia/rest/controller/ClienteController.java
@@ -5,22 +5,17 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import io.github.ErickFernandes_Algnologia.domain.entity.Cliente;
 import io.github.ErickFernandes_Algnologia.domain.repository.Clientes;
+import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+@RestController// Serve para n ter que usar o ResponseBody nos Metodos
+@RequestMapping("/api/clientes")
 //@RequestMapping("/api/clientes")
 public class ClienteController {
 
@@ -35,65 +30,65 @@ public class ClienteController {
      * = RequestMethod.GET, consumes = { "application/json", "application/xml "},
      * produces = { "application/json", "application/xml"} )
      */
-    @GetMapping("/api/clientes/{id}")
-    @ResponseBody // Vai transformar esse retorno em um objeto do tipo Json
-    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+    @GetMapping("{id}")
+    //@ResponseBody // Vai transformar esse retorno em um objeto do tipo Json
+    public Cliente ClientegetClienteById(@PathVariable Integer id) {
 
-        Optional<Cliente> cliente = clientes.findById(id);
+        return this.clientes
+                        .findById(id)
+                        .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Cliente nao encontrado") );
 
-        if (cliente.isPresent()) {
-            return ResponseEntity.ok(cliente.get());//200
-        }
-
-        return ResponseEntity.notFound().build();//retorna o erro 404
     }
 
-    @PostMapping("/api/clientes")//serve para quando for salvar um recurso no servidor, ou seja, enviar informações que não existem lá no servidor, utiliza-se o Post.
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente) {
+    @PostMapping//serve para quando for salvar um recurso no servidor, ou seja, enviar informações que não existem lá no servidor, utiliza-se o Post.
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente) {
 
-        Cliente clienteSalvo = this.clientes.save(cliente);
-
-        return ResponseEntity.ok(clienteSalvo);
+        return this.clientes.save(cliente);
 
     };
 
-    @DeleteMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id) {
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
 
-        Optional<Cliente> cliente = clientes.findById(id);
+        clientes.
+                findById(id)
+                .map( cliente -> {
 
-        if (cliente.isPresent()) {
-            clientes.delete(cliente.get());
-            return ResponseEntity.noContent().build();
-        }
+                    clientes.delete(cliente);
+                    return cliente;
 
-
-        return ResponseEntity.notFound().build();
-
+                }).orElseThrow( () -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Cliente não encontrado"
+        ));
 
     }
 
-    @PutMapping("/api/clientes/{id}")// o put ele é usado para atualizar integralmente
-    @ResponseBody
-    public ResponseEntity update(
+    @PutMapping("{id}")// o put ele é usado para atualizar integralmente
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(
         @PathVariable Integer id, 
         @RequestBody Cliente  cliente) {
 
-        return clientes
+        clientes
         .findById(id)
         .map( clienteExistente -> {
             cliente.setId(clienteExistente.getId());
             clientes.save(cliente);
-            return ResponseEntity.noContent().build();
-        }).orElseGet(() -> ResponseEntity.notFound().build());//supplier
+            return clienteExistente;
+        }).orElseThrow( () -> new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Cliente não encontrado"
+        ));
 
     }
 
 
-    @GetMapping("/api/clientes")
-    public ResponseEntity find(Cliente filtro) {
+    @GetMapping
+    public List<Cliente> find(Cliente filtro) {
 
         ExampleMatcher matcher = ExampleMatcher
                                     .matching()
@@ -104,10 +99,7 @@ public class ClienteController {
 
         Example example = Example.of(filtro, matcher);
 
-        List<Cliente> lista = this.clientes.findAll(example);
-
-        return ResponseEntity.ok(lista);
-
+        return this.clientes.findAll(example);
     }
 
     /*
